0c202af928dd010f09f277371382c45599a4615c
renata-blzk <123456789+renata.brogaite@users.noreply.github.com>
2024-01-14 15:22:49 +0000

    Add getComputerChoice function

--- myScript.js
+++ myScript.js
@@ -1,1 +1,14 @@
-console.log("Hello, world!");
+const game = Math.floor(Math.random() * 3);
+if (game === 0) {
+    console.log("Rock");
+} else if (game === 1) {
+    console.log("Papper");
+} else {
+    console.log("Scissors");
+}
+
+function getComputerChoice() {
+    return game;
+}
+
+getComputerChoice();
