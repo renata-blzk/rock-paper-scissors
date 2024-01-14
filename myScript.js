ec755c446880d5777a49ffa176dde67d43462282
renata-blzk <123456789+renata.brogaite@users.noreply.github.com>
2024-01-14 19:01:12 +0000

    Update GetComputerChoice code

--- myScript.js
+++ myScript.js
@@ -1,25 +1,37 @@
 // Random Computer choice
 
-const input = Math.floor(Math.random() * 3);
-if (input === 0) {
-    console.log("Rock");
-} else if (input === 1) {
-    console.log("Papper");
-} else {
-    console.log("Scissors");
-}
 
 function getComputerChoice() {
-    console.log(input);
+    return (Math.floor(Math.random() *3));
 }
+
+if (getComputerChoice() === 0) {
+    alert("Rock");
+} else if (getComputerChoice() === 1) {
+    alert("Paper");
+} else {
+    alert("Scissors");
+}
+
+getComputerChoice();
+
+//let input = Math.floor(Math.random() * 3);
+//if (input === 0) {
+//    console.log("Rock");
+//} else if (input === 1) {
+//    console.log("Papper");
+//} else {
+//    console.log("Scissors");
+//}
+
 
 
 // Player section
 
-let choice = prompt("Please choose Rock, Paper or Scissors:");
+//let choice = prompt("Please choose Rock, Paper or Scissors:");
 
-const computerSelection = getComputerChoice();
+//const computerSelection = getComputerChoice();
 
-if (choice != null) {
-    alert(input);
-}
+//if (choice != null) {
+//    alert(input);
+//}
