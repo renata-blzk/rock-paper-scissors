4da3091755ea965223e1ada788bca24151531714
renata-blzk <123456789+renata.brogaite@users.noreply.github.com>
2024-05-26 18:50:09 +0100

    tidy faults in js

--- myScript.js
+++ myScript.js
@@ -1,78 +1,5 @@
-/*let playRoundResut;
-let playerScore = 0;
-let computerScore = 0;
-
-    const playGame = () =>  {
-    for (let i = 0; i < 5; i++) {
-    let choice = Math.floor(Math.random() * 3) +1;
-
-    function getComputerChoice() {
-        if (choice === 1) return 'ROCK';
-        if (choice === 2) return 'PAPER';
-        return 'SCISSORS';
-    }
-    
-    function playRound(playerSelection, computerSelection) {
-        const win = (computerSelection === 'ROCK' && playerSelection === 'PAPER') ||
-        (computerSelection === 'SCISSORS' && playerSelection === 'ROCK') ||
-        (computerSelection === 'PAPER' && playerSelection === 'SCISSORS');
-        if (computerSelection === playerSelection) return 'Tie!';
-        if (win) {
-            playerScore++;
-            return 'You won!';
-        }
-        computerScore++;
-        return 'You loose!'; 
-    }
-
-    const playerSelection = player.toUpperCase();
-    const computerSelection = getComputerChoice();
-    playRoundResult = playRound(playerSelection, computerSelection);
-    console.log(playRound(playerSelection, computerSelection));
-
-    console.log(`Player pick: ${playerSelection}`);
-    console.log(`Computer pick: ${computerSelection}`);
-
-    }
-
-playGame();
-
-console.log(`Computer ${computerScore}`);
-console.log(`Player ${playerScore}`);*/
-
-
-/*const buttons = document.querySelectorAll("button");
-buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-    
-  });
-});*/
-
-
-//get computer choice
-let choice = Math.floor(Math.random() * 3) +1;
-function getComputerChoice() {
-    if (choice === 1) return 'ROCK';
-    if (choice === 2) return 'PAPER';
-    return 'SCISSORS';
-}
-
-//check who won the game
-function playRound(playerSelection, computerSelection) {
-    const win = (computerSelection === 'ROCK' && playerSelection === 'PAPER') ||
-    (computerSelection === 'SCISSORS' && playerSelection === 'ROCK') ||
-    (computerSelection === 'PAPER' && playerSelection === 'SCISSORS');
-    if (computerSelection === playerSelection) return 'Tie!';
-    if (win) {
-        playerScore++;
-        return 'You won!';
-    }
-    computerScore++;
-    return 'You loose!'; 
-}
-
-let playerScore = 0;
-let computerScore = 0;
+let playerCount = 0;
+let computerCount = 0;
 let playerSelection;
 let computerSelection;
 let gameInProgress = true;
@@ -81,78 +8,101 @@ const rock = document.querySelector("#btn1");
 const paper = document.querySelector("#btn2");
 const scissors = document.querySelector("#btn3");
 
+//get computer choice
+function getComputerChoice() {
+    const choice = Math.floor(Math.random() * 3) +1;
+    if (choice === 1) return 'ROCK';
+    if (choice === 2) return 'PAPER';
+    return 'SCISSORS';
+}
 
-rock.addEventListener("click", () => {
+//check who won the game
+function playRound(playerSelection, computerSelection) {
+    const win = (computerSelection === 'ROCK' && playerSelection === 'PAPER') ||
+                (computerSelection === 'SCISSORS' && playerSelection === 'ROCK') ||
+                (computerSelection === 'PAPER' && playerSelection === 'SCISSORS');
+    if (computerSelection === playerSelection) return 'Tie!';
+    if (win) {
+        playerCount++;
+        return 'You won!';
+    }
+    computerCount++;
+    return 'You loose!'; 
+}
+
+function handlePlayerSelection(selection) {
     if (gameInProgress) {
-        playerSelection = "ROCK";
+        playerSelection = selection;
         game();
-        //highlight the option clicked by user
-        rock.classList.add('highlight');
+        highlight(selection);
         overlay();
     }
-});
+}
 
-paper.addEventListener("click", () => {
-    if (gameInProgress) {
-        playerSelection = "PAPER";
-        game();
-        paper.classList.add('highlight');
-        overlay();
-    }
-});
-
-scissors.addEventListener("click", () => {
-    if (gameInProgress) {
-        playerSelection = "SCISSORS";
-        game();
-        rock.classList.add('highlight');
-        overlay();
-    }
-});
-
-document.addEventListener('mouseup', (e) => {
+function highlight(selection) {
     rock.classList.remove('highlight');
     paper.classList.remove('highlight');
     scissors.classList.remove('highlight');
-});
+    if (selection == 'ROCK') rock.classList.add('highlight');
+    if (selection == 'PAPER') paper.classList.add('highlight');
+    if (selection == 'SCISSORS') scissors.classList.add('highlight');
+}
+
+rock.addEventListener("click", () => handlePlayerSelection('ROCK'));
+paper.addEventListener("click", () => handlePlayerSelection('PAPER'));
+scissors.addEventListener("click", () => handlePlayerSelection('SCISSORS'));
 
 function game() {
-    computerSeletion = getComputerChoice();
+    computerSelection = getComputerChoice();
 
     //div that displays who won
     let result =  document.getElementById('details');
     
     //button that displays computer's choice
     let computerChoice = document.querySelector('.btn-computer');
+    computerChoice.textContent = computerSelection;
 
-    computerChoice.textContent = computerSelection.toUpperCase();
-    result.textContent = playRound (playerSelection, computerSelection).toUpperCase();
-    let humanScore =document.querySelector('.human');
-    humanScore.textContent = playerCount;
+    result.textContent - playRound(playerSelection, computerSelection);
+
+    let humanScore = document.querySelector('.human');
+    //if (humanScore) {
+        humanScore.textContent = playerCount;
+    //}
+
     let computerScore = document.querySelector('.computer');
-    computerScore.textContent = computerCount;
-
-    
+    if (computerScore) {
+        computerScore.textContent = computerCount;   
+    }
 }
 
 function overlay() {
     if (playerCount === 5) {
         let finalResult = document.getElementById('details');
-        finalResult.textContent = 'You Won';
+        finalResult.textContent = playerCount === 5 ? 'You Won' : 'You Lost';
 
         playAgain();
         gameInProgress = false;
     }
-
-    else if (computerCount === 5) {
-        let finalResult = document.getElementById('details');
-        finalResult.textContent = 'You Lost';
-
-        playAgain();
-        gameInProgress = false;
-    }
-
 }
 
+function playAgain() {
+    let btnNew = document.createElement('button');
+    btnNew.textContent = 'Play Again';
+    let appendBtn = document.getElementById('details');
+    btnNew.style.backgroundColor = 'silver';
+    appendBtn.appendChild(btnNew);
 
-
+    btnNew.addEventListener('click', () => {
+        playerCount = 0;
+        computerCount = 0;
+        finalResult = undefined;
+        let humanScore = document.querySelector('.human');
+        humanScore.textContent = playerCount;
+        let computerScore = document.querySelector('.computer');
+        if (computerScore) {
+            computerScore.textContent = computerCount;
+        }
+        appendBtn.removeChild(btnNew);
+        gameInProgress = true;
+    });
+}
